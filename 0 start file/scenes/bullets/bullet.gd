8b13aef40e2e76f93c75c774c025e99827ed43bf
beni <mamont4444@live.ru>
2026-02-06 18:38:27 +0200

    feat: Add initial Godot project setup including core scenes (player, level, bullet), scripts, and various game assets.

---
+++ 0 start file/scenes/bullets/bullet.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+class_name Bullet
+
+@export var speed: float = 200.0
+var direction: Vector2 
+		
+func setup(pos: Vector2, dir: Vector2):
+	print ('pos: ',pos)
+	print ('dir: ',dir)
+
+	position = pos + dir * 16
+	direction = dir
+	rotation = direction.angle()
+	
+	
+func _physics_process(delta: float) -> void:
+		position += direction * speed * delta
---
+++ 0 start file/scenes/entities/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+
+var direction_x: float
+@export var speed := 1000
+var can_shoot = true
+@export var jump_strength :=400
+@export var gravity := 1000
+
+signal shoot(pos: Vector2, dir:Vector2)
+
+func get_input():
+	direction_x = Input.get_axis("left", "right")
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_strength		
+	if Input.is_action_just_pressed("shoot") and $ReloadTimer.time_left == 0:
+		shoot.emit(position, get_local_mouse_position().normalized())
+		can_shoot = false
+		$ReloadTimer.start()
+		
+		
+		
+func apply_gravity(delta):	
+	velocity.y += gravity * delta
+
+func _physics_process(delta: float) -> void:
+	
+	get_input()
+	apply_gravity(delta)
+
+	if direction_x:
+		velocity.x = direction_x * speed
+		
+	move_and_slide()
+	
