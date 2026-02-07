d81a7558cd28f27280775ea0858e2e24a219daf1
beni <mamont4444@live.ru>
2026-02-07 13:03:47 +0200

    bullet scale

--- 0 start file/scenes/bullets/bullet.gd
+++ 0 start file/scenes/bullets/bullet.gd
@@ -10,7 +10,14 @@ func setup(pos: Vector2, dir: Vector2):
 	position = pos + dir * 16
 	direction = dir
 	rotation = direction.angle()
+
+func _ready() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Sprite2D, "scale", Vector2.ONE, 0.5).from(Vector2.ZERO)
 	
 	
 func _physics_process(delta: float) -> void:
 		position += direction * speed * delta
+		
+		
+		
