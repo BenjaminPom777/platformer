38fa2a63048c85b630af29e7cf7a164a73634dc3
beni <mamont4444@live.ru>
2026-02-13 12:18:28 +0200

    fixed shader bug

--- 0 start file/scenes/entities/drone.gd
+++ 0 start file/scenes/entities/drone.gd
@@ -10,16 +10,16 @@ var exploded := false
 
 func _ready() -> void:
 	$ExplosionSprite.hide()
+	$AnimatedSprite2D.material = $AnimatedSprite2D.material.duplicate()
 
 func _physics_process(_delta: float) -> void:
 	if player:
 		var dir = (player.position - position).normalized()
 		velocity = dir * speed
-		$AnimatedSprite2D.flip_h = dir.x > 0 
+		$AnimatedSprite2D.flip_h = dir.x > 0
 		move_and_slide()
 		
 
-		
 func _on_detection_area_body_entered(player_body: CharacterBody2D) -> void:
 	player = player_body
 
@@ -36,7 +36,7 @@ func explode():
 	$CollisionShape2D.set_deferred("disabled", true)
 	$AnimatedSprite2D.hide()
 	$ExplosionSprite.show()
-	$AnimationPlayer.play("explode")	
+	$AnimationPlayer.play("explode")
 	$AudioStreamPlayer2D.play()
 	await $AnimationPlayer.animation_finished
 	
@@ -46,19 +46,17 @@ func chain_reaction():
 	for drone in get_tree().get_nodes_in_group('Drones') as Array[Drone]:
 		var distance = position.distance_to(drone.position)
 		if distance <= 50 && drone != self:
-			drone.explode()			
+			drone.explode()
 
 func _on_collision_area_body_entered(_player_body: CharacterBody2D) -> void:
 	explode()
 
 
-	
 func hit():
 	var tween = create_tween()
 	tween.tween_property($AnimatedSprite2D.material, 'shader_parameter/Progress', 0.1, 0.5)
 	tween.tween_property($AnimatedSprite2D.material, 'shader_parameter/Progress', 1, 0.5)
 	
 	health -= 1
-	if health <=0:
+	if health <= 0:
 		explode()
-	
